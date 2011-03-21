4c9acdb30ced9831db67a6d86988bbcc19fe8fed
rmichelli <rmichelli@nc.rr.com>
2011-03-21 12:06:01 -0400

    Clean up whitespace

--- Ext.ux.form.LovCombo.js
+++ Ext.ux.form.LovCombo.js
@@ -1,82 +1,82 @@
-﻿// vim: ts=4:sw=4:nu:fdc=4:nospell 
+﻿// vim: ts=4:sw=4:nu:fdc=4:nospell
 /*global Ext */
-/** 
-* @class Ext.ux.form.LovCombo 
-* @extends Ext.form.ComboBox 
-* 
-* Simple list of values Combo 
-* 
-* @author Ing. Jozef Sakáloš 
-* @copyright (c) 2008, by Ing. Jozef Sakáloš 
-* @version 1.3 
-* @date <ul> 
-* <li>16. April 2008</li> 
-* <li>2. February 2009</li> 
-* </ul> 
-* @revision $Id: Ext.ux.form.LovCombo.js 618 2009-03-08 23:47:48Z jozo $ 
-* 
-* @license Ext.ux.form.LovCombo.js is licensed under the terms of the Open Source 
-* LGPL 3.0 license. Commercial use is permitted to the extent that the 
-* code/component(s) do NOT become part of another Open Source or Commercially 
-* licensed development library or toolkit without explicit permission. 
-* 
-* <p>License details: <a href="http://www.gnu.org/licenses/lgpl.html" 
-* target="_blank">http://www.gnu.org/licenses/lgpl.html</a></p> 
-* 
-* @forum 32692 
-* @demo http://lovcombo.extjs.eu 
-* @download 
-* <ul> 
-* <li><a href="http://lovcombo.extjs.eu/lovcombo.tar.bz2">lovcombo.tar.bz2</a></li> 
-* <li><a href="http://lovcombo.extjs.eu/lovcombo.tar.gz">lovcombo.tar.gz</a></li> 
-* <li><a href="http://lovcombo.extjs.eu/lovcombo.zip">lovcombo.zip</a></li> 
-* </ul> 
-* 
-* @donate 
-* <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"> 
-* <input type="hidden" name="cmd" value="_s-xclick"> 
-* <input type="hidden" name="hosted_button_id" value="3430419"> 
-* <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif" 
-* border="0" name="submit" alt="PayPal - The safer, easier way to pay online."> 
-* <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> 
-* </form> 
+/**
+ * @class Ext.ux.form.LovCombo
+ * @extends Ext.form.ComboBox
+ *
+ * Simple list of values Combo
+ *
+ * @author Ing. Jozef Sakáloš
+ * @copyright (c) 2008, by Ing. Jozef Sakáloš
+ * @version 1.3
+ * @date <ul>
+ * <li>16. April 2008</li>
+ * <li>2. February 2009</li>
+ * </ul>
+ * @revision $Id: Ext.ux.form.LovCombo.js 618 2009-03-08 23:47:48Z jozo $
+ *
+ * @license Ext.ux.form.LovCombo.js is licensed under the terms of the Open Source
+ * LGPL 3.0 license. Commercial use is permitted to the extent that the
+ * code/component(s) do NOT become part of another Open Source or Commercially
+ * licensed development library or toolkit without explicit permission.
+ *
+ * <p>License details: <a href="http://www.gnu.org/licenses/lgpl.html"
+ * target="_blank">http://www.gnu.org/licenses/lgpl.html</a></p>
+ *
+ * @forum 32692
+ * @demo http://lovcombo.extjs.eu
+ * @download
+ * <ul>
+ * <li><a href="http://lovcombo.extjs.eu/lovcombo.tar.bz2">lovcombo.tar.bz2</a></li>
+ * <li><a href="http://lovcombo.extjs.eu/lovcombo.tar.gz">lovcombo.tar.gz</a></li>
+ * <li><a href="http://lovcombo.extjs.eu/lovcombo.zip">lovcombo.zip</a></li>
+ * </ul>
+ *
+ * @donate
+ * <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+ * <input type="hidden" name="cmd" value="_s-xclick">
+ * <input type="hidden" name="hosted_button_id" value="3430419">
+ * <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif"
+ * border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+ * <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+ * </form>
 */
 
-// Check RegExp.escape dependency 
+// Check RegExp.escape dependency
 if ('function' !== typeof RegExp.escape) {
     throw ('RegExp.escape function is missing. Include Ext.ux.util.js file.');
 }
 
-// create namespace 
+// create namespace
 Ext.ns('Ext.ux.form');
 
-/** 
-* Creates new LovCombo 
-* @constructor 
-* @param {Object} config A config object 
+/**
+* Creates new LovCombo
+* @constructor
+* @param {Object} config A config object
 */
 Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
 
-    // {{{ 
-    // configuration options 
-    /** 
-    * @cfg {String} checkField Name of field used to store checked state. 
-    * It is automatically added to existing fields. 
-    * (defaults to "checked" - change it only if it collides with your normal field) 
+    // {{{
+    // configuration options
+    /**
+    * @cfg {String} checkField Name of field used to store checked state.
+    * It is automatically added to existing fields.
+    * (defaults to "checked" - change it only if it collides with your normal field)
     */
     checkField: 'checked'
 
-    /** 
-    * @cfg {String} separator Separator to use between values and texts (defaults to "," (comma)) 
+    /**
+    * @cfg {String} separator Separator to use between values and texts (defaults to "," (comma))
     */
 , separator: ','
 
-    /** 
-    * @cfg {String/Array} tpl Template for items. 
-    * Change it only if you know what you are doing. 
+    /**
+    * @cfg {String/Array} tpl Template for items.
+    * Change it only if you know what you are doing.
     */
-    // }}} 
-    // {{{ 
+    // }}}
+    // {{{
 , constructor: function(config) {
     config = config || {};
     config.listeners = config.listeners || {};
@@ -86,12 +86,12 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
 , blur: this.onRealBlur
     });
     Ext.ux.form.LovCombo.superclass.constructor.call(this, config);
-} // eo function constructor 
-    // }}} 
-    // {{{ 
+} // eo function constructor
+    // }}}
+    // {{{
 , initComponent: function() {
 
-    // template with checkbox 
+    // template with checkbox
     if (!this.tpl) {
         this.tpl =
 '<tpl for=".">'
@@ -105,10 +105,10 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
 ;
     }
 
-    // call parent 
+    // call parent
     Ext.ux.form.LovCombo.superclass.initComponent.apply(this, arguments);
 
-    // remove selection from input field 
+    // remove selection from input field
     this.onLoad = this.onLoad.createSequence(function() {
         if (this.el) {
             var v = this.el.dom.value;
@@ -117,24 +117,24 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
         }
     });
 
-} // eo function initComponent 
-    // }}} 
-    // {{{ 
-    /** 
-    * Disables default tab key bahavior 
-    * @private 
+} // eo function initComponent
+    // }}}
+    // {{{
+    /**
+    * Disables default tab key bahavior
+    * @private
     */
 , initEvents: function() {
     Ext.ux.form.LovCombo.superclass.initEvents.apply(this, arguments);
 
-    // disable default tab handling - does no good 
+    // disable default tab handling - does no good
     this.keyNav.tab = false;
 
-} // eo function initEvents 
-    // }}} 
-    // {{{ 
-    /** 
-    * Clears value 
+} // eo function initEvents
+    // }}}
+    // {{{
+    /**
+    * Clears value
     */
 , clearValue: function() {
     this.value = '';
@@ -147,28 +147,28 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
         this.hiddenField.value = '';
     }
     this.applyEmptyText();
-} // eo function clearValue 
-    // }}} 
-    // {{{ 
-    /** 
-    * @return {String} separator (plus space) separated list of selected displayFields 
-    * @private 
+} // eo function clearValue
+    // }}}
+    // {{{
+    /**
+    * @return {String} separator (plus space) separated list of selected displayFields
+    * @private
     */
 , getCheckedDisplay: function() {
     var re = new RegExp(this.separator, "g");
     return this.getCheckedValue(this.displayField).replace(re, this.separator + ' ');
-} // eo function getCheckedDisplay 
-    // }}} 
-    // {{{ 
-    /** 
-    * @return {String} separator separated list of selected valueFields 
-    * @private 
+} // eo function getCheckedDisplay
+    // }}}
+    // {{{
+    /**
+    * @return {String} separator separated list of selected valueFields
+    * @private
     */
 , getCheckedValue: function(field) {
     field = field || this.valueField;
     var c = [];
 
-    // store may be filtered so get all records 
+    // store may be filtered so get all records
     var snapshot = this.store.snapshot || this.store.data;
 
     snapshot.each(function(r) {
@@ -178,22 +178,22 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
     }, this);
 
     return c.join(this.separator);
-} // eo function getCheckedValue 
-    // }}} 
-    // {{{ 
-    /** 
-    * beforequery event handler - handles multiple selections 
-    * @param {Object} qe query event 
-    * @private 
+} // eo function getCheckedValue
+    // }}}
+    // {{{
+    /**
+    * beforequery event handler - handles multiple selections
+    * @param {Object} qe query event
+    * @private
     */
 , onBeforeQuery: function(qe) {
     qe.query = qe.query.replace(new RegExp(RegExp.escape(this.getCheckedDisplay()) + '[ ' + this.separator + ']*'), '');
-} // eo function onBeforeQuery 
-    // }}} 
-    // {{{ 
-    /** 
-    * blur event handler - runs only when real blur event is fired 
-    * @private 
+} // eo function onBeforeQuery
+    // }}}
+    // {{{
+    /**
+    * blur event handler - runs only when real blur event is fired
+    * @private
     */
 , onRealBlur: function() {
     this.list.hide();
@@ -202,7 +202,7 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
     var va = [];
     var snapshot = this.store.snapshot || this.store.data;
 
-    // iterate through raw values and records and check/uncheck items 
+    // iterate through raw values and records and check/uncheck items
     Ext.each(rva, function(v) {
         snapshot.each(function(r) {
             if (v === r.get(this.displayField)) {
@@ -212,36 +212,36 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
     }, this);
     this.setValue(va.join(this.separator));
     this.store.clearFilter();
-} // eo function onRealBlur 
-    // }}} 
-    // {{{ 
-    /** 
-    * Combo's onSelect override 
-    * @private 
-    * @param {Ext.data.Record} record record that has been selected in the list 
-    * @param {Number} index index of selected (clicked) record 
+} // eo function onRealBlur
+    // }}}
+    // {{{
+    /**
+    * Combo's onSelect override
+    * @private
+    * @param {Ext.data.Record} record record that has been selected in the list
+    * @param {Number} index index of selected (clicked) record
     */
 , onSelect: function(record, index) {
     if (this.fireEvent('beforeselect', this, record, index) !== false) {
 
-        // toggle checked field 
+        // toggle checked field
         record.set(this.checkField, !record.get(this.checkField));
 
-        // display full list 
+        // display full list
         if (this.store.isFiltered()) {
             this.doQuery(this.allQuery);
         }
 
-        // set (update) value and fire event 
+        // set (update) value and fire event
         this.setValue(this.getCheckedValue());
         this.fireEvent('select', this, record, index);
     }
-} // eo function onSelect 
-    // }}} 
-    // {{{ 
-    /** 
-    * Sets the value of the LovCombo 
-    * @param {Mixed} v value 
+} // eo function onSelect
+    // }}}
+    // {{{
+    /**
+    * Sets the value of the LovCombo
+    * @param {Mixed} v value
     */
 , setValue: function(v) {
     if (v) {
@@ -276,35 +276,35 @@ Ext.ux.form.LovCombo = Ext.extend(Ext.form.ComboBox, {
     else {
         this.clearValue();
     }
-} // eo function setValue 
-    // }}} 
-    // {{{ 
-    /** 
-    * Selects all items 
+} // eo function setValue
+    // }}}
+    // {{{
+    /**
+    * Selects all items
     */
 , selectAll: function() {
     this.store.each(function(record) {
-        // toggle checked field 
+        // toggle checked field
         record.set(this.checkField, true);
     }, this);
 
-    //display full list 
+    //display full list
     this.doQuery(this.allQuery);
     this.setValue(this.getCheckedValue());
-} // eo full selectAll 
-    // }}} 
-    // {{{ 
-    /** 
-    * Deselects all items. Synonym for clearValue 
+} // eo full selectAll
+    // }}}
+    // {{{
+    /**
+    * Deselects all items. Synonym for clearValue
     */
 , deselectAll: function() {
     this.clearValue();
-} // eo full deselectAll 
-    // }}} 
+} // eo full deselectAll
+    // }}}
 
-}); // eo extend 
+}); // eo extend
 
-// register xtype 
+// register xtype
 Ext.reg('lovcombo', Ext.ux.form.LovCombo);
 
 // eof
